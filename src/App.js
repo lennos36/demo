a09869b36889285861b89109b41a534ff33cf80c
lennos36 <shane.lennon36@mail.dcu.ie>
2021-03-06 17:41:13 +0000

    adding css for user chat

--- src/App.js
+++ src/App.js
@@ -78,9 +78,12 @@ function App() {
       <div className="messages">
         <div className="messages-scroller">
           { state.messages.map((message, index) => (
-            <div key={ message.id } className={message.user === 'shane' ? 'message me' : 'message'}>{ message.user }
-              <div>{ message.message } </div>
+            <div key={ message.id }>
+              <div> { message.user }</div>
+              <div className={message.user === 'shane' ? 'message me' : 'message'}>
+                <div>{ message.message }</div>
                 <div className="date"> { moment(message.createdAt).format('HH:mm')}</div>
+              </div>
             </div>
           ))}
         </div>
